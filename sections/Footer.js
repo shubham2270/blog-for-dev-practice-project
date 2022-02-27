716b4ee6b8ad6df2b21a0ccdca1b2ccc9bf943ed
Shubham Kumar <shubham.kumar@aicrowd.com>
2022-02-27 12:08:02 +0530

    add footer, header, layout

---
+++ sections/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='px-4 sm:px-6 mt24'>
+      <p className='text-center text-sm text-gray-500'>
+        {new Date().getFullYear} AlterClass. Al rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./sections/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
